59486e03560dd9287391a316fd1d12402d304e23
XXRV0423 <xxavierrv@gmail.com>
2026-08-22 11:51:16 -0400

    Fix: added missing attributes in the constructor

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,7 +1,7 @@
 import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft) {
+    constructor(sqft, floors) {
         super(sqft);
         this._floors = floors;
     }
