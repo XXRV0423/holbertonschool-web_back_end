b11db8257e97c397853f5170d33fb28c53d6497b
XXRV0423 <xxavierrv@gmail.com>
2026-08-26 16:12:26 -0400

    Create creageInt8TypedArray function using DataView to set Int8 value at position

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+function createInt8TypedArray(lenght, position, value) {
+    const buffer = new ArrayBuffer(lenght);
+    const view = new DataView(buffer);
+
+    try {
+        view.setInt8(position, value);
+    } catch (error) {
+        throw new Error('Position outside range');
+    }
+
+    return view;
+}
