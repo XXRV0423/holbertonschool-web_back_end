9003f38e83dca2266bb9eb1188f47398ae9dc195
XXRV0423 <xxavierrv@gmail.com>
2026-08-26 17:09:11 -0400

    Create updateUniqueItems function updating map entries with quantity of 1

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+function updateUniqueItems(map) {
+    if (!(map instanceof Map)) {
+        throw new Error('Cannot process');
+    }
+
+    map.forEach((quantity, name) => {
+        if (quantity === 1) {
+            map.set(name, 100);
+        }
+    });
+
+    return map
+}
+
+export default updateUniqueItems;
