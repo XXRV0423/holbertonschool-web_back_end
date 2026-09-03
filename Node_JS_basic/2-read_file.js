82f15824f129b401d0249c783745bf9386db9dfe
XXRV0423 <xxavierrv@gmail.com>
2026-09-03 15:40:56 -0400

    Fix 2-read_file.js: add hasOwnProperty guard to for-in loop for lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -9,7 +9,6 @@ function countStudents(path) {
   }
 
   const lines = data.split('\n').filter((line) => line.trim() !== '');
-  // first line is the header
   const students = lines.slice(1);
 
   const fields = {};
@@ -28,8 +27,10 @@ function countStudents(path) {
   console.log(`Number of students: ${students.length}`);
 
   for (const field in fields) {
-    const list = fields[field];
-    console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+    if (Object.prototype.hasOwnProperty.call(fields, field)) {
+      const list = fields[field];
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+    }
   }
 }
 
